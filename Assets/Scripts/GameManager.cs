c3820daf1c0c8dd301806aee1710c9506077cbac
RaderRMT <raderpy@protonmail.com>
2024-05-15 10:39:17 +0200

    TMP + pause screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     
     public bool IsPaused { get; private set; }
+    public Action<bool> OnPauseStateChanged;
 
     private void Awake()
     {
@@ -16,6 +18,7 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             IsPaused = !IsPaused;
+            OnPauseStateChanged.Invoke(IsPaused);
         }
     }
 }
--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -4,10 +4,21 @@ public class GameUI : MonoBehaviour
 {
     public static GameUI Instance;
 
+    public GameObject PauseScreen;
     public MoonPhaseProgress MoonPhaseProgress;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnPauseStateChanged += OnPause;
+    }
+
+    private void OnPause(bool isPaused)
+    {
+        PauseScreen.SetActive(isPaused);
     }
 }
