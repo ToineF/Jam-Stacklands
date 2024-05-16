7d5708bb60c130ae96c27bdfa87198ae095bc55a
RaderRMT <raderpy@protonmail.com>
2024-05-16 15:26:58 +0200

    fix cards

--- Assets/Scripts/UI/Phases/DemonsLeavingPhase.cs
+++ Assets/Scripts/UI/Phases/DemonsLeavingPhase.cs
@@ -4,7 +4,19 @@ public class DemonsLeavingPhase : MonoBehaviour
 {
     private void Update()
     {
-        // todo: spawn humans
+        for (int i = GameManager.Instance.CurrentCards.Count; i > 0; i--) {
+            Card card = GameManager.Instance.CurrentCards[i];
+            if (card.Data.Type != CardData.CardType.Demonic)
+            {
+                continue;
+            }
+
+            if (!card.HasCommitedMurder)
+            {
+                GameManager.Instance.CurrentCards.Remove(card);
+                Destroy(card.gameObject);
+            }
+        }
         
         GameUI.Instance.MoonPhaseProgress.GameState = MoonPhaseProgress.State.DEMONS_LEAVE;
     }
