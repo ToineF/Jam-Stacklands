d245e5ced53e5fdf1e29f37b2c77a2385e846f54
RaderRMT <raderpy@protonmail.com>
2024-05-16 10:51:51 +0200

    added main meny

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Transform dynamicSlow;
+    [SerializeField]
+    private Transform dynamicFast;
+    [SerializeField]
+    private Image toHide;
+
+    [SerializeField]
+    private float dynamicSlowDistance;
+    [SerializeField]
+    private float dynamicFastDistance;
+    
+    [SerializeField]
+    private float dynamicSpeed;
+
+    private bool _isTweening;
+
+    private void Update()
+    {
+        if (_isTweening)
+        {
+            return;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _isTweening = true;
+            dynamicSlow.DOLocalMoveX(dynamicSlowDistance, dynamicSpeed, true).OnComplete(() => _isTweening = false);
+            dynamicFast.DOLocalMoveX(dynamicFastDistance, dynamicSpeed, true).OnComplete(() => _isTweening = false);
+            toHide.DOFade(0, dynamicSpeed);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _isTweening = true;
+            dynamicSlow.DOLocalMoveX(0, dynamicSpeed, true).OnComplete(() => _isTweening = false);
+            dynamicFast.DOLocalMoveX(0, dynamicSpeed, true).OnComplete(() => _isTweening = false);
+            toHide.DOFade(1, dynamicSpeed);
+        }
+    }
+}
