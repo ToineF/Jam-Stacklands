ac3453ed64c147fb3d7835b578ee2da93da2cb46
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2024-05-17 00:53:48 +0200

    [PROGRAMMING] Audio Manager + Musics 🎶

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioSource _sfxSource;
+    [SerializeField] private AudioSource _mainMusicSource;
+    [SerializeField] private float _mainMusicAudioFadeTime;
+
+    private void Awake()
+    {
+        Instance = this;
+        StartFade(_mainMusicSource, _mainMusicAudioFadeTime, 1);
+    }
+
+    public void PlayClip(AudioClip clip)
+    {
+        _sfxSource.PlayOneShot(clip);
+    }
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
+    }
+}
